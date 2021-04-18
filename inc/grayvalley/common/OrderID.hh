31a891df9db2b6c06ec4d150db94ce29bdc887ae
Juha Hellen <juha.hellen@aalto.fi>
2021-04-18 19:59:10 +0300

    add: check

--- inc/grayvalley/common/OrderID.hh
+++ inc/grayvalley/common/OrderID.hh
@@ -2,12 +2,17 @@
 #define GVT_ORDERID_HH
 
 #include <string>
+#include <stdexcept>
 
 class OrderID {
 public:
     const std::string value;
 public:
-    explicit OrderID(const std::string& orderID): value{orderID}{};
+    explicit OrderID(const std::string& orderID): value{orderID}{
+        if (orderID.empty()){
+            throw std::runtime_error("OrderID invalid.");
+        }
+    };
 };
 
 #endif //AMM_ORDERID_HH
